11a32462760d9b91f4d5af6228df5baf77dab6af
kyungseogkim <86609170+kyungseog@users.noreply.github.com>
2023-02-02 23:56:36 +0900

    Update sheet_korea.js

--- google/sheet_korea.js
+++ google/sheet_korea.js
@@ -10,10 +10,10 @@ const koreaSheetId = util.lib.sheetIds.koreaSheetId;
 const rateRange = 'rate!A2:C10000';
 const supplierRange = 'supplier!A2:D10000';
 const brandRange = 'brand!A2:H10000';
-const customerRange = 'customer!A2:B500000';
+const customerRange = 'customer!A2:C500000';
 const liveRange = 'live!A2:F10000';
 const stockRange = 'stock!A2:L20000';
-const colorCodeRange = 'color_code!A2:C500000';
+const costCodeRange = 'cost_code!A2:C500000';
 
 const client = new google.auth.JWT(
   keys.client_email,
@@ -28,22 +28,37 @@ client.authorize(function(err, tokens){
         return;
     } else {
         console.log('GoogleSheet Connected!');
-        gsRead(client);
+        updateKoreaData(client);
     }
 });
 
-async function gsRead(client) {
+async function updateKoreaData(client) {
+  const updateArray = [
+    [rateRange, exchange_rate],
+    [supplierRange, suppliers],
+    [brandRange, brands],
+    [customerRange, korea_users],
+    [liveRange, live_commerces],
+    [stockRange, stocks],
+    [costCodeRange, cost_ids]
+  ];
+
   const gsapi = google.sheets({version : 'v4', auth : client});
-  const readOption = {
+
+  for ( let i = 0; i < updateArray.length; i++ ) {
+    const options = {
       spreadsheetId: koreaSheetId,
-      range: rateRange
-  };
-  let data = await gsapi.spreadsheets.values.get(readOption);
-  let dataArray = data.data.values;
+      range: updateArray[i][0]
+    };
+    let datas = await gsapi.spreadsheets.values.get(options);
+    let dataArray = datas.data.values;
 
-  const dataSql = insertSql.korea_rate;
+    const sql = insertSql[updateArray[i][1]];
 
-  util.param.db.query(dataSql, [dataArray], function(error, result) {
-    error? console.log(error): console.log('update korea data');
-  });
+    util.param.db.query(sql, [dataArray], function(error, result) {
+      error ? console.log(error) : console.log('update korea rate data');
+    });
+    await util.delayTime(1000);
+  }
+
 }
