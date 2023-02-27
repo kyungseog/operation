f9ef8f7bdda8ed9193817076a491484df33b3242
kyungseogkim <86609170+kyungseog@users.noreply.github.com>
2023-02-27 23:49:22 +0900

    Create weather_korea_actual.js

---
+++ public/weather_korea_actual.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const { DateTime } = require("luxon");
+const util = require("../data-center/utility.js");
+
+// const rule = new util.lib.schedule.RecurrenceRule();
+// rule.dayOfWeek = [0, 1, 2, 3, 4, 5, 6];
+// rule.hour = 7;
+// rule.minute = 0;
+
+// util.lib.schedule.scheduleJob( "getActualWeather", rule, () => actualWeather() );
+actualWeather();
+async function actualWeather() {
+  const targetDate = DateTime.now().minus({days: 1}).toFormat('yyyyLLdd');
+console.log(util.param.weather_key)
+  const paramDetail = util.lib.qs.stringify({
+    serviceKey: util.param.weather_key, 
+    numOfRows: 100, 
+    dataType: 'JSON', 
+    dataCd: 'ASOS',
+    dateCd: 'DAY',
+    startDt: targetDate,
+    endDt: targetDate,
+    stnIds: 108
+  });
+  
+  const options = { 
+    method: 'GET',
+    url: `http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?${paramDetail}`
+  };
+  
+  const actualData = await util.requestData(options);
+  console.log(actualData);
+  let actualArray = actualData.response.body.items.item;
+  let uploadData = actualArray.map(u => ['KR', 'seoul', u.tm, u.minTa, u.maxTa, 0, 0, u.sumRn, u.ddMes]);
+  
+  const sql = `
+    INSERT INTO management.weather
+      (country
+      , city
+      , date
+      , temperature_min
+      , temperature_max
+      , estimate_am
+      , estimate_pm
+      , rain
+      , snow) 
+    VALUES ?
+    ON DUPLICATE KEY UPDATE 
+      temperature_min=values(temperature_min)
+      , temperature_max=values(temperature_max)
+      , estimate_am = estimate_am
+      , estimate_pm = estimate_pm
+      , rain=values(rain)
+      , snow=values(snow)`;
+  
+  util.param.db.query(sql, [uploadData], function(error, result) {
+    error ? console.log(error) : console.log('update actualData');
+  });
+};
