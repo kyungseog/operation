36da72dabd81157110b7d2e89c8094334d2d1c41
kskim <kskim@kskimui-MacBookPro.local>
2023-03-20 20:11:48 +0900

    update

--- godo/allocation_fees.js
+++ godo/allocation_fees.js
@@ -5,14 +5,14 @@ const util = require("../data-center/utility.js");
 
 (async function start() {
   let targetDate = [];
-  for (let i = 1; i <= 28; i++) {
+  for (let i = 16; i <= 19; i++) {
     let day = i < 10 ? "0" + i : i;
-    targetDate.push("2023-02-" + day);
+    targetDate.push("2023-03-" + day);
   }
 
   for (let i = 0; i < targetDate.length; i++) {
-    // const marketing = await allocationMarketingFees(targetDate[i]);
-    // console.log("marketing", marketing);
+    const marketing = await allocationMarketingFees(targetDate[i]);
+    console.log("marketing", marketing);
     const logistic = await calculationLogisticFees(targetDate[i]);
     console.log("logistic", logistic);
     await util.delayTime(1000);
