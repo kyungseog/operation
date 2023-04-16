d7c6db9ae2119d23cf4174f6fbb361f0987e278e
김경석 <kevin_kim@cpe-172-100-0-12.twcny.res.rr.com>
2023-04-17 00:08:44 +0900

    update google analytics data

--- google/analytics.js
+++ google/analytics.js
@@ -5,22 +5,17 @@ const { BetaAnalyticsDataClient } = require("@google-analytics/data");
 const analyticsDataClient = new BetaAnalyticsDataClient();
 // Runs a simple report.
 async function runRealtimeReport() {
-  const [response] = await analyticsDataClient.runRealtimeReport({
+  const response = await analyticsDataClient.runRealtimeReport({
     property: `properties/${250906029}`,
-    minuteRanges: [
-      {
-        name: "0-4 minutes ago",
-        start_minutes_ago: 4,
-      },
-    ],
+    minuteRanges: [{ startMinutesAgo: 29 }],
     dimensions: [{ name: "country" }],
     metrics: [{ name: "activeUsers" }],
   });
 
-  console.log("Report result:");
-  response.rows.forEach((row) => {
-    console.log(row.dimensionValues[0], row.metricValues[0]);
-  });
+  console.log(response[0].rows);
+  // response.rows.forEach((row) => {
+  //   console.log(row.dimensionValues[0].value, row.metricValues[0].value);
+  // });
 }
 
 runRealtimeReport();
