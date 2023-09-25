c3842747393dc85e853c50d8f0c32fed8357a07c
김경석 <kevin_kim@cpe-172-100-0-12.twcny.res.rr.com>
2023-09-26 01:06:32 +0900

    update cafe

---
+++ cafe/products.js
@@ -0,0 +1,161 @@
+"use strict";
+
+const util = require("../data-center/utility.js");
+const { DateTime } = require("luxon");
+
+const ruleKR = new util.lib.schedule.RecurrenceRule();
+ruleKR.dayOfWeek = [1, 2, 3, 4, 5, 6];
+ruleKR.minute = 20;
+util.lib.schedule.scheduleJob("productsKR", ruleKR, () => updateProductKR());
+
+const ruleJP = new util.lib.schedule.RecurrenceRule();
+ruleJP.dayOfWeek = [1, 2, 3, 4, 5, 6];
+ruleJP.minute = 50;
+util.lib.schedule.scheduleJob("productsJP", ruleJP, () => updateProductJP());
+
+async function updateProductKR() {
+  const token = await util.sqlData(`SELECT access_token FROM cmipdb.i_cafe24auth`);
+  const setHeaders = {
+    Authorization: "Bearer " + token[token.length - 1].access_token,
+    "Content-Type": "application/json",
+    "X-Cafe24-Api-Version": "2022-06-01",
+  };
+
+  const countOptions = {
+    method: "GET",
+    url: `https://moomooz.cafe24api.com/api/v2/admin/products/count`,
+    headers: setHeaders,
+  };
+
+  const resCountData = await util.requestData(countOptions);
+  const latestProductNo = resCountData.count + 100000;
+
+  const fieldData =
+    "product_no,product_code,custom_product_code,product_name,supply_product_name,price,display,selling,tax_type,list_image,manufacturer_code,trend_code,brand_code,supplier_code,created_date,updated_date";
+  console.log("kr product data update start");
+
+  for (let i = 0; 100 * i < latestProductNo; i++) {
+    const productOptions = {
+      method: "GET",
+      url: `https://moomooz.cafe24api.com/api/v2/admin/products?since_product_no=${
+        100 * i
+      }&fields=${fieldData}&limit=100`,
+      headers: setHeaders,
+    };
+    const resProductData = await util.requestData(productOptions);
+    const productsArray = resProductData.products;
+
+    if (productsArray != undefined) {
+      const productDataArray = productsArray.map((u) => [
+        u.product_no,
+        u.product_code,
+        u.custom_product_code,
+        u.product_name,
+        u.supply_product_name,
+        u.price,
+        u.display,
+        u.selling,
+        u.tax_type,
+        u.list_image,
+        u.manufacturer_code,
+        u.trend_code,
+        u.brand_code,
+        u.supplier_code,
+        new Date(Date.parse(u.created_date)),
+        new Date(Date.parse(u.updated_date)),
+      ]);
+
+      const productDataSql = `INSERT INTO cmipdb.api_productdata (product_no,product_code,custom_product_code,
+        product_name,supply_product_name,price,display,selling,
+        tax_type,list_image,manufacturer_code,trend_code,brand_code,supplier_code,created_date,updated_date) 
+      VALUES ? ON DUPLICATE KEY UPDATE product_code=values(product_code), custom_product_code=values(custom_product_code), 
+        product_name=values(product_name), supply_product_name=values(supply_product_name), price=values(price), 
+        display=values(display), selling=values(selling), tax_type=values(tax_type), 
+        list_image=values(list_image), manufacturer_code=values(manufacturer_code),
+        trend_code=values(trend_code), brand_code=values(brand_code), supplier_code=values(supplier_code), 
+        created_date=values(created_date), updated_date=values(updated_date), update_cnt=update_cnt+1`;
+
+      util.sqlData(productDataSql, [productDataArray]);
+    }
+    util.delayTime(1000);
+  }
+  console.log("kr product data update complete");
+}
+
+async function updateProductJP() {
+  const token = await util.sqlData(`SELECT access_token FROM i_cafe24auth`);
+  const setHeaders = {
+    Authorization: "Bearer " + token[token.length - 1].access_token,
+    "Content-Type": "application/json",
+    "X-Cafe24-Api-Version": "2022-06-01",
+  };
+  const countOptions = {
+    method: "GET",
+    url: `https://moomooz.cafe24api.com/api/v2/admin/products/count?shop_no=2`,
+    headers: setHeaders,
+  };
+
+  const resCountData = await util.requestData(countOptions);
+  const latestProductNo = resCountData.count + 100000;
+
+  const fieldData =
+    "product_no,product_code,custom_product_code,product_name,supply_product_name,eng_product_name,internal_product_name,price,display,selling,tax_type,list_image,manufacturer_code,trend_code,brand_code,supplier_code,hscode,clearance_category_kor,clearance_category_code,english_product_material,cloth_fabric,created_date,updated_date";
+  console.log("jp product data update start");
+
+  for (let i = 0; 100 * i < latestProductNo; i++) {
+    const productOptions = {
+      method: "GET",
+      url: `https://moomooz.cafe24api.com/api/v2/admin/products?shop_no=2&since_product_no=${
+        100 * i
+      }&fields=${fieldData}&limit=100`,
+      headers: setHeaders,
+    };
+
+    const resProductData = await util.requestData(productOptions);
+    const productsArray = resProductData.products;
+
+    if (productsArray != undefined) {
+      const productDataArray = productsArray.map((u) => [
+        u.product_no,
+        u.product_code,
+        u.custom_product_code,
+        u.product_name,
+        u.supply_product_name,
+        u.eng_product_name,
+        u.price,
+        u.display,
+        u.selling,
+        u.tax_type,
+        u.list_image,
+        u.manufacturer_code,
+        u.trend_code,
+        u.brand_code,
+        u.supplier_code,
+        u.hscode,
+        u.clearance_category_kor,
+        u.clearance_category_code,
+        u.english_product_material,
+        u.cloth_fabric,
+        new Date(Date.parse(u.created_date)),
+        new Date(Date.parse(u.updated_date)),
+      ]);
+
+      const productDataSql = `INSERT INTO cmipdb.api_productdata_jp (product_no,product_code,custom_product_code,
+        product_name,supply_product_name,eng_product_name,price,display,selling,
+        tax_type,list_image,manufacturer_code,trend_code,brand_code,supplier_code,
+        hscode,clearance_category_kor,clearance_category_code,english_product_material,cloth_fabric,created_date,updated_date) 
+      VALUES ? ON DUPLICATE KEY UPDATE product_code=values(product_code), custom_product_code=values(custom_product_code), 
+        product_name=values(product_name), supply_product_name=values(supply_product_name), eng_product_name=values(eng_product_name), price=values(price), 
+        display=values(display), selling=values(selling), tax_type=values(tax_type), 
+        list_image=values(list_image), manufacturer_code=values(manufacturer_code),
+        trend_code=values(trend_code), brand_code=values(brand_code), supplier_code=values(supplier_code),
+        hscode=values(hscode), clearance_category_kor=values(clearance_category_kor), clearance_category_code=values(clearance_category_code), 
+        english_product_material=values(english_product_material), cloth_fabric=values(cloth_fabric),
+        created_date=values(created_date), updated_date=values(updated_date)`;
+
+      util.sqlData(productDataSql, [productDataArray]);
+    }
+    util.delayTime(1000);
+  }
+  console.log("jp product data update complete");
+}
--- cafe/sales_jp_minute.js
+++ cafe/sales_jp_minute.js
@@ -1,1 +1,207 @@
 "use strict";
+
+const util = require("../data-center/utility.js");
+const { DateTime } = require("luxon");
+
+const rule15 = new util.lib.schedule.RecurrenceRule();
+rule15.dayOfWeek = [0, 1, 2, 3, 4, 5, 6];
+rule15.minute = 15;
+util.lib.schedule.scheduleJob("jpSales15", rule15, () => controlOrderUpdate());
+
+const rule45 = new util.lib.schedule.RecurrenceRule();
+rule45.dayOfWeek = [0, 1, 2, 3, 4, 5, 6];
+rule45.minute = 45;
+util.lib.schedule.scheduleJob("jpSales45", rule45, () => controlOrderUpdate());
+
+async function controlOrderUpdate() {
+  const token = await util.sqlData(`SELECT access_token FROM i_cafe24auth`);
+  const setHeaders = {
+    Authorization: "Bearer " + token[token.length - 1].access_token,
+    "Content-Type": "application/json",
+    "X-Cafe24-Api-Version": "2022-03-01",
+  };
+
+  console.log(DateTime.now().toFormat("yyyy-LL-dd HH:mm:ss"), "jp order data update start");
+
+  const day = DateTime.now().toFormat("dd");
+
+  if (day < 16) {
+    for (let i = 0; i < day; i++) {
+      const targetDate = DateTime.now().minus({ days: i }).toFormat("yyyy-LL-dd");
+      await countOrders(setHeaders, targetDate);
+      util.delayTime(4000);
+    }
+    console.log(DateTime.now().toFormat("yyyy-LL-dd HH:mm:ss"), "jp order data update complete");
+  } else {
+    for (let i = 0; i < 16; i++) {
+      const targetDate = DateTime.now().minus({ days: i }).toFormat("yyyy-LL-dd");
+      await countOrders(setHeaders, targetDate);
+      util.delayTime(4000);
+    }
+    for (let i = 16; i < day; i++) {
+      const targetDate = DateTime.now().minus({ days: i }).toFormat("yyyy-LL-dd");
+      await countOrders(setHeaders, targetDate);
+      util.delayTime(4000);
+    }
+    console.log(DateTime.now().toFormat("yyyy-LL-dd HH:mm:ss"), "jp order data update complete");
+  }
+}
+
+async function countOrders(setHeaders, targetDate) {
+  const countOptions = {
+    method: "GET",
+    url: `https://moomooz.cafe24api.com/api/v2/admin/orders/count?shop_no=2&start_date=${targetDate}&end_date=${targetDate}&date_type=pay_date`,
+    headers: setHeaders,
+  };
+
+  const resCountData = await util.requestData(countOptions);
+  const orderCount = resCountData.count;
+
+  if (orderCount !== 0) {
+    if (orderCount % 500 == 0) {
+      for (let j = 1; j < orderCount / 500 + 1; j++) {
+        await updateDatas(setHeaders, targetDate, j);
+      }
+    } else {
+      for (let j = 1; j < parseInt(orderCount / 500) + 2; j++) {
+        await updateDatas(setHeaders, targetDate, j);
+      }
+    }
+  }
+}
+
+async function updateDatas(setHeaders, targetDate, j) {
+  const orderOptions = {
+    method: "GET",
+    url: `https://moomooz.cafe24api.com/api/v2/admin/orders?shop_no=2&start_date=${targetDate}&end_date=${targetDate}&date_type=pay_date&embed=items,buyer,receivers&limit=500&offset=${
+      500 * (j - 1)
+    }`,
+    headers: setHeaders,
+  };
+
+  const resOrderData = await util.requestData(orderOptions);
+  let ordersArray = resOrderData.orders;
+
+  let orderDataArray = [];
+  const accountCode = ["N10", "N20", "N21", "N22", "N30", "N40", "E00", "E10", "N01", "E12", "E13", "E20"];
+
+  //일본몰 초기 배송번호 오류를 수정하기위한 내역
+  const errorOrderItemCode = [
+    "20220617-0001129-04",
+    "20220616-0021415-03",
+    "20220616-0021415-04",
+    "20220616-0023293-02",
+    "20220616-0003053-03",
+    "20220616-0000524-02",
+    "20220616-0000524-04",
+    "20220616-0000524-05",
+    "20220615-0016695-06",
+    "20220614-0003438-02",
+    "20220614-0002713-01",
+    "20220614-0002713-02",
+    "20220614-0002713-03",
+    "20220615-0010723-01",
+    "20220615-0010723-04",
+    "20220614-0027447-02",
+    "20220614-0028918-02",
+    "20220613-0014471-05",
+    "20220613-0014471-10",
+    "20220612-0006246-10",
+    "20220613-0005948-04",
+    "20220612-0015819-06",
+    "20220610-0018144-02",
+  ];
+  const fixTrackingNo = [
+    "SEP22062203341",
+    "SEP22062203354",
+    "SEP22062203354",
+    "SEP22062303649",
+    "SEP22062002751",
+    "SEP22062303673",
+    "SEP22062303673",
+    "SEP22062303673",
+    "SEP22062002744",
+    "SEP22062303665",
+    "SEP22062002747",
+    "SEP22062002747",
+    "SEP22062002747",
+    "SEP22062002746",
+    "SEP22062002746",
+    "SEP22062002742",
+    "SEP22062303663",
+    "SEP22062002740",
+    "SEP22062002740",
+    "SEP22062203337",
+    "SEP22061602108",
+    "SEP22061702348",
+    "SEP22062103147",
+  ];
+
+  for (let k = 0; k < ordersArray.length; k++) {
+    let items = ordersArray[k].items;
+    let receiver = ordersArray[k].receivers;
+    for (let l = 0; l < items.length; l++) {
+      let data = [
+        new Date(Date.parse(ordersArray[k].order_date)),
+        new Date(Date.parse(ordersArray[k].payment_date)),
+        items[l].shipped_date === null ? null : new Date(Date.parse(items[l].shipped_date)),
+        ordersArray[k].order_id,
+        items[l].order_item_code,
+        ordersArray[k].member_id,
+        ordersArray[k].buyer.phone,
+        ordersArray[k].buyer.cellphone,
+        items[l].status_text,
+        items[l].order_status,
+        items[l].supplier_id,
+        items[l].custom_product_code,
+        items[l].custom_variant_code,
+        items[l].product_no,
+        items[l].product_code,
+        items[l].product_name,
+        items[l].variant_code,
+        items[l].option_value,
+        parseInt(items[l].product_price),
+        items[l].option_price,
+        items[l].quantity,
+        parseInt(items[l].additional_discount_price),
+        errorOrderItemCode.indexOf(items[l].order_item_code) >= 0
+          ? fixTrackingNo[errorOrderItemCode.indexOf(items[l].order_item_code)]
+          : items[l].tracking_no,
+        items[l].product_tax_type,
+        ordersArray[k].first_order,
+        ordersArray[k].paid,
+        ordersArray[k].group_no_when_ordering,
+        receiver[0].cellphone,
+        receiver[0].phone,
+        receiver[0].zipcode,
+        accountCode.indexOf(items[l].order_status) >= 0 ? "T" : "F",
+        items[l].product_bundle,
+      ];
+      orderDataArray.push(data);
+    }
+  }
+
+  const orderDataSql = `INSERT INTO apidb.api_orderdata_jp (order_date,payment_date,shipped_date,order_id,
+    order_item_code,member_id,member_phone,member_cellphone,status_text,order_status,
+    supplier_code,custom_product_code,custom_variant_code,product_no,product_code,product_name,
+    variant_code,option_value,product_price,option_price,quantity,
+    additional_discount_price,tracking_no,product_tax_type,first_order,paid,group_no_when_ordering,
+    receiver_cellphone,receiver_phone,receiver_zipcode,account_code,product_bundle)
+  VALUES ? ON DUPLICATE KEY UPDATE order_date=values(order_date), payment_date=values(payment_date),
+    shipped_date=values(shipped_date), order_id=values(order_id), member_id=values(member_id), 
+    member_phone=values(member_phone), member_cellphone=values(member_cellphone), status_text=values(status_text), 
+    order_status=values(order_status), supplier_code=values(supplier_code), custom_product_code=values(custom_product_code), 
+    custom_variant_code=values(custom_variant_code),product_no=values(product_no), 
+    product_code=values(product_code), product_name=values(product_name), variant_code=values(variant_code), 
+    option_value=values(option_value), product_price=values(product_price),
+    option_price=values(option_price), quantity=values(quantity), additional_discount_price=values(additional_discount_price), 
+    tracking_no=values(tracking_no), product_tax_type=values(product_tax_type), 
+    first_order=values(first_order), paid=values(paid), group_no_when_ordering=values(group_no_when_ordering), 
+    receiver_cellphone=values(receiver_cellphone), receiver_phone=values(receiver_phone), receiver_zipcode=values(receiver_zipcode), 
+    account_code=values(account_code), product_bundle=values(product_bundle)`;
+
+  util.sqlData(orderDataSql, [orderDataArray]);
+
+  orderDataArray = [];
+  ordersArray = [];
+}
